467f45cdf40365c2f63e548551c00bff15a8ec04
Joe Tustin <J.Tustin@gmail.com>
2020-07-26 10:37:59 -0600

    adding in first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hi There!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain("Hi There!");
+
+  ReactDOM.unmountComponentAtNode(div);
 });
