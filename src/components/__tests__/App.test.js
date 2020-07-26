ebbedf801cae60a64e292d2d89e6afb509f3b2f0
Joe Tustin <J.Tustin@gmail.com>
2020-07-26 13:53:35 -0600

    adding in jest linter

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../App";
+
+it("shows a comment box", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(<App />, div);
+
+  //   Looks inside div
+  // checks to see if the CommentBox is in there
+
+  expect(div.innerHTML).toContain("Comment Box");
+
+  ReactDOM.unmountComponentAtNode(div);
+  //   clean up function
+});
